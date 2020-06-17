19ca96ad0f4967f19f2dbc3a04e4cc7a16522870
Bárbara <babyr.oliveira@gmail.com>
2020-06-17 20:40:48 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DoadorSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" ----- POSSO SER DOADOR DE SANGUE? ----- \n");
+                    
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Qual a sua idade? ");
+            bool idade2 = Int32.TryParse(Console.ReadLine(), out idade);
+
+             if (idade >=18 && idade <=67)
+                 Console.WriteLine("Você pode ser doador de sangue.");
+
+             else
+                Console.WriteLine("Você não pode ser doador de sangue.");
+
+            Console.ReadKey();
+
+        }  
+    }
+}
